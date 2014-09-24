3d3c4d756437abc042c38b88caf44dbb29e28f79
Juan A. Suarez Romero <jasuarez@igalia.com>
2014-09-24 13:59:26 +0200

    Allow custom "Next" button per page
    Allow pages to set a custom text for the forward button, instead of
    default "Next"
    
    [endlessm/eos-shell#3594]

--- gnome-initial-setup/gis-page.h
+++ gnome-initial-setup/gis-page.h
@@ -67,6 +67,8 @@ GType gis_page_get_type (void);
 
 char *       gis_page_get_title (GisPage *page);
 void         gis_page_set_title (GisPage *page, char *title);
+const char *       gis_page_get_forward_text (GisPage *page);
+void         gis_page_set_forward_text (GisPage *page, const char *text);
 gboolean     gis_page_get_complete (GisPage *page);
 void         gis_page_set_complete (GisPage *page, gboolean complete);
 gboolean     gis_page_get_skippable (GisPage *page);
