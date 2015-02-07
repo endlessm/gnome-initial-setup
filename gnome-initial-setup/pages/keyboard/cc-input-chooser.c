1501040edcfcda780ea6dcd449ee171fde29b10b
Cosimo Cecchi <cosimo@endlessm.com>
2015-02-06 16:10:59 -0800

    input-chooser: sort default layout before others
    (Rebase 3.38: This could potentially be dropped in future if we update
    cc-input-chooser.c from gnome-control-center; it’s copy/paste code.)

--- gnome-initial-setup/pages/keyboard/cc-input-chooser.c
+++ gnome-initial-setup/pages/keyboard/cc-input-chooser.c
@@ -482,6 +482,8 @@ sort_inputs (GtkListBoxRow *a,
                 gpointer       data)
 {
         InputWidget *la, *lb;
+        CcInputChooser *chooser = data;
+        CcInputChooserPrivate *priv = cc_input_chooser_get_instance_private (chooser);
 
         la = get_input_widget (gtk_list_box_row_get_child (a));
         lb = get_input_widget (gtk_list_box_row_get_child (b));
@@ -491,6 +493,12 @@ sort_inputs (GtkListBoxRow *a,
 
         if (lb == NULL)
                 return -1;
+
+        if (g_strcmp0 (priv->id, la->id) == 0)
+                return -1;
+
+        if (g_strcmp0 (priv->id, lb->id) == 0)
+                return 1;
 
         if (la->is_extra && !lb->is_extra)
                 return 1;
