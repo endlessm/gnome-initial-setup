14553d35379c44b5a109e6342df126192a3210d2
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2016-09-21 02:11:57 -0300

    assistant: add API to show/hide the forward button
    Because the Live Installer page has all the options inside the
    page, it's confusing and unnecessary to show an insensitive
    "Next" button at the headerbar.
    
    This API will be used in the future to implement the Live Installer
    page.
    
    Also, add an autoptr cleanup function in page to allow us to use
    the new G_DECLARE_{FINAL,DERIVABLE}_TYPE facilities and eventually
    remove some boilerplate code.
    
    https://phabricator.endlessm.com/T12547

--- gnome-initial-setup/gis-assistant.c
+++ gnome-initial-setup/gis-assistant.c
@@ -227,8 +227,8 @@ update_navigation_buttons (GisAssistant *assistant)
     }
   else
     {
-      gboolean is_first_page;
       GtkWidget *next_widget;
+      gboolean can_go_forward, is_first_page, hide_forward_button;
 
       is_first_page = (page_priv->link->prev == NULL);
       gtk_widget_set_visible (priv->back, !is_first_page);
@@ -238,7 +238,13 @@ update_navigation_buttons (GisAssistant *assistant)
       else
         next_widget = priv->forward;
 
-      if (gis_page_get_complete (page)) {
+      can_go_forward = gis_page_get_complete (page);
+      gtk_widget_set_sensitive (priv->forward, can_go_forward);
+
+      hide_forward_button = gis_page_get_hide_forward_button (priv->current_page);
+      gtk_widget_set_visible (priv->forward, !hide_forward_button);
+
+      if (can_go_forward) {
         set_suggested_action_sensitive (next_widget, TRUE);
         set_navigation_button (assistant, next_widget);
       } else if (gis_page_get_skippable (page)) {
