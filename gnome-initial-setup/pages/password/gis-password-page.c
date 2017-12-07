endlessm/gnome-initial-setup

commit 04ab16bb51e0ee0c2db86885c86c618c8c41340f

Revert "password: Enable Show Password by default"
This reverts commit deef21c0a7175a1435f2ef5b65da9a5b3acaf4eb.

gnome-initial-setup/pages/password/gis-password-page.c
@@ -57,7 +57,7 @@ typedef struct _GisPasswordPagePrivate GisPasswordPagePrivate;
 G_DEFINE_TYPE_WITH_PRIVATE (GisPasswordPage, gis_password_page, GIS_TYPE_PAGE);
 
 static void
-update_password_visibility (GisPasswordPage  *page)
+password_visibility_toggled (GisAccountPage  *page)
 {
   GisPasswordPagePrivate *priv = gis_password_page_get_instance_private (page);
   GtkWidget *password_entry = priv->password_entry;
@@ -66,13 +66,6 @@ update_password_visibility (GisPasswordPage  *page)
 
   gtk_entry_set_visibility (GTK_ENTRY (password_entry), is_active);
   gtk_entry_set_visibility (GTK_ENTRY (confirm_entry), is_active);
-}
-
-static void
-password_visibility_toggled (GtkToggleButton *button,
-                             GisPasswordPage  *page)
-{
-  update_password_visibility (page);
 }
 
 static gboolean
@@ -307,7 +300,6 @@ gis_password_page_constructed (GObject *object)
 
   g_signal_connect (priv->password_toggle, "toggled",
                     G_CALLBACK (password_visibility_toggled), page);
-  update_password_visibility (page);
 
   g_signal_connect (GIS_PAGE (page)->driver, "notify::username",
                     G_CALLBACK (username_changed), page);

gnome-initial-setup/pages/password/gis-password-page.ui
@@ -197,7 +197,6 @@
                 <property name="visible">True</property>
                 <property name="can_focus">True</property>
                 <property name="receives_default">True</property>
-                <property name="active">True</property>
               </object>
               <packing>
                 <property name="left_attach">2</property>

commit 41cd37cda49788541946f365d1c72587240eb82c

password: fix wrong type in function signature
This was introduced in commit 04ab16bb51e0ee0c2db86885c86c618c8c41340f.

gnome-initial-setup/pages/password/gis-password-page.c
@@ -60,7 +60,7 @@ typedef struct _GisPasswordPagePrivate GisPasswordPagePrivate;
 G_DEFINE_TYPE_WITH_PRIVATE (GisPasswordPage, gis_password_page, GIS_TYPE_PAGE);
 
 static void
-password_visibility_toggled (GisAccountPage  *page)
+password_visibility_toggled (GisPasswordPage  *page)
 {
   GisPasswordPagePrivate *priv = gis_password_page_get_instance_private (page);
   GtkWidget *password_entry = priv->password_entry;