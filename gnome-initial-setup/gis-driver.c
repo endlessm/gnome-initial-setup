1a94089c93836d88a76e6d2de6dc9f7009438d26
Will Thompson <will@willthompson.co.uk>
2018-02-22 19:15:06 +0000

    language: Launch eos-installer on eosinstaller images
    If install.ini exists, we pre-set the locale from it.
    
    If unattended.ini exists, we pre-set the locale from it, and immediately launch eos-installer.
    In this case, we want to avoid gnome-initial-setup ever being
    shown to the user. So, we explicitly hide it, and teach GisDriver to not
    present() the window if it has been hidden.
    
    Otherwise, we wait for the user to click Next.
    
    To avoid actually advancing to the next page, we (ab)use
    the ->apply() vfunc, which is intended to be used by pages like the
    keyboard page which need to asynchronously change system settings when
    you hit Next, delaying the transition to the next page, and blocking it
    in the event that the operation fails. In this case, the asynchronous
    operation is to launch eos-installer, wait for it to terminate, and
    always report that ->apply() failed.
    
    (Rebase 3.38: Fix minor rebase conflicts.)
    (Rebase 41.2: Fix minor rebase conflicts.)
    (Rebase 43: Combine two related patches.)
    
    https://phabricator.endlessm.com/T17082

--- gnome-initial-setup/gis-driver.c
+++ gnome-initial-setup/gis-driver.c
@@ -106,6 +106,7 @@ struct _GisDriver {
   GisDriverMode mode;
   UmAccountMode account_mode;
   gboolean small_screen;
+  gboolean hidden;
 
   locale_t locale;
 
@@ -611,12 +612,14 @@ gis_driver_add_page (GisDriver *driver,
 void
 gis_driver_show_window (GisDriver *driver)
 {
+  driver->hidden = FALSE;
   gtk_window_present (driver->main_window);
 }
 
 void
 gis_driver_hide_window (GisDriver *driver)
 {
+  driver->hidden = TRUE;
   gtk_widget_hide (GTK_WIDGET (driver->main_window));
 }
 
@@ -854,7 +857,8 @@ gis_driver_activate (GApplication *app)
 
   G_APPLICATION_CLASS (gis_driver_parent_class)->activate (app);
 
-  gtk_window_present (GTK_WINDOW (driver->main_window));
+  if (!driver->hidden)
+    gtk_window_present (GTK_WINDOW (driver->main_window));
 }
 
 static void
